368421f90622e49baa8fec6f783b9f5c22e8e76d
minh1304 <vtm1304@gmail.com>
2022-08-14 23:47:18 +0700

    upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var trangthai = "duoi150";
+    var menu = document.querySelector('.navbar')
+    console.log(menu);
+
+    window.addEventListener('scroll',function(){
+        if(this.window.pageYOffset >150)
+        {
+            if(trangthai = "duoi150")
+            {
+                trangthai = "tren150";
+                menu.classList.add('giunguyen')
+
+            }
+        }
+        else if(window.pageYOffset <=150)
+        {
+            if(trangthai="tren150")
+            {
+                menu.classList.remove('giunguyen');
+                trangthai="duoi150";
+            }
+        }
+    })
+},false)
