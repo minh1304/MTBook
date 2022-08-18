11b4f1664764728d4e767258028e594d6af26192
minh1304 <vtm1304@gmail.com>
2022-08-18 15:32:40 +0700

    upload

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded",function(){
     var trangthai = "duoi150";
     var menu = document.querySelector('.navbar');
     var hienbang = document.querySelector('._1khoi .tel .icon img');
-    var bang = document.querySelector('.bang');
 
-    console.log(hienbang);
+    var bang = document.querySelector('.bang');
+    var closebang = document.querySelector('.bang .nutclose');
+    var test = document.querySelector('body');
+
 
     window.addEventListener('scroll',function(){
         if(this.window.pageYOffset >150)
@@ -27,9 +29,17 @@ document.addEventListener("DOMContentLoaded",function(){
     })
 
     hienbang.onclick = function() {
+        // test2.classList.add('black');
         bang.classList.add('hienbang1');
+        test.classList.add('black');
+        // bang.classList.remove('black');
 
-    }
+    };
+    closebang.onclick = function (){ 
+        bang.classList.remove('hienbang1');
+        test.classList.remove('black');
+
+    };
 
 
 
