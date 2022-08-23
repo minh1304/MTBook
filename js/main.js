12179f30752e75f6d14912fb22bd38e542e2c77b
minh1304 <vtm1304@gmail.com>
2022-08-23 15:45:15 +0700

    upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
 document.addEventListener("DOMContentLoaded",function(){
     var trangthai = "duoi150";
     var menu = document.querySelector('.navbar');
-    var hienbang1 = document.querySelector('._1khoi .tel .icon img.icontel');
-    var hienbang2 = document.querySelector('._1khoi .tel .icon img.iconemail');
-    var bang1 = document.querySelector('.row .col .bang1');
-    console.log(bang1);
-    var bang2 = document.querySelector('.row .col .bang2');
-    var closebang1 = document.querySelector('.row .col .bang1 .nutclose1');
-    console.log(closebang1);
-    var closebang2 = document.querySelector('.row .col .bang2 .nutclose2');
-    console.log(closebang2);
-    var thongtin = document.getElementById('name');
-    var test = document.getElementById('test');
-    console.log(test);
+    var nut = document.querySelector('.pushdy-widget-button.pd-tel');
+    var noidunghienlen = document.querySelector('.pd-overlay');
+    console.log(noidunghienlen);
 
-
-
-    // var test = document.querySelector('body');
+    var tatnoidung = document.querySelector('.pd-close-tawk');
 
 
     window.addEventListener('scroll',function(){
@@ -39,33 +28,17 @@ document.addEventListener("DOMContentLoaded",function(){
         }
     })
 
-    hienbang1.onclick = function() {
-        // bang2.classList.remove('hienbang');
-        bang1.classList.add('hienbang');
-        bang2.classList.remove('hienbang');
+
+    nut.addEventListener('click',function(){
+        console.log('ahihi');
+        noidunghienlen.style.display = 'block';
 
 
 
-    };
-    hienbang2.onclick = function() {
-        // test2.classList.add('black');
+    });
+    tatnoidung.onclick = function() {
+        noidunghienlen.style.display = 'none';
 
-        bang2.classList.add('hienbang');
-        bang1.classList.remove('hienbang');
-
-
-
-    };
-    closebang1.onclick = function (){ 
-        bang1.classList.remove('hienbang');
-
-    };
-    closebang2.onclick = function (){ 
-        bang2.classList.remove('hienbang');
-
-    };
-
-
-
+    }
 
 },false)
